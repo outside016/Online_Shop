65fbcadcf8748bea87043b6d8fbe3b9d56b974ef
outside <outside016@icloud.com>
2023-11-15 23:49:15 +0300

    work get

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Items} from "../../models/items";
+import {Subscription} from "rxjs";
+import {ItemsService} from "../../services/items.service";
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.scss']
 })
-export class ItemsComponent {
+export class ItemsComponent implements OnInit{
 
+  items!: Items[];
+
+  itemsSubscriprion!: Subscription;
+  constructor(private ItemsService: ItemsService) {
+  }
+  ngOnInit() {
+    this.itemsSubscriprion = this.ItemsService.getItems().subscribe((data)=>
+    this.items = data)
+  }
+
+  ngOnDestroy(){
+    if (this.itemsSubscriprion) this.itemsSubscriprion.unsubscribe()
+  }
 }
